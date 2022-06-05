20ed2bbb0c5114a0e72f1b966541d6119af7e928
LuanBMartins <luan.cbm@hotmail.com>
2022-06-05 01:04:29 -0300

    fix: remove conflicts

--- src/interface/gym-student/gym-student-model.ts
+++ src/interface/gym-student/gym-student-model.ts
@@ -1,8 +1,5 @@
 export default interface GymStudentModel {
-<<<<<<< HEAD
     name: string;
-=======
->>>>>>> 40bf0ddaf424faebf47b12a7462ebcbe58abf71f
     id: number;
     email: string;
     password: string;
--- src/main/routes/gym-student-routes.ts
+++ src/main/routes/gym-student-routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express'
 import ExpressRouterAdapter from '../adapter/express-route-adapter'
-<<<<<<< HEAD
 import GymStudentCreateRouterComposer from '../composer/gym-student/gym-student-create-composer'
 import GymStudentLoadCompose from '../composer/gym-student/gym-student-load-composer'
 import GymStudentDeleteRouterComposer from '../composer/gym-student/gym-student-delete-composer'
@@ -11,10 +10,4 @@ module.exports = (router: Router) => {
   router.post('/gymstudent/load', ExpressRouterAdapter.adapt(GymStudentLoadCompose.composer()))
   router.post('/gymstudent/delete', ExpressRouterAdapter.adapt(GymStudentDeleteRouterComposer.composer()))
   router.post('/gymstudent/update/:id', ExpressRouterAdapter.adapt(GymStudentupdateRoute.composer()))
-=======
-import GymStudentCreateRouterComposer from '../composer/gym-student/gym-student-composer'
-
-module.exports = (router: Router) => {
-  router.post('/gymstudent/create', ExpressRouterAdapter.adapt(GymStudentCreateRouterComposer.composer()))
->>>>>>> 40bf0ddaf424faebf47b12a7462ebcbe58abf71f
 }
