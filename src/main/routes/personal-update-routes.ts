7f0d84ca60519685754d146bfbf796d96135d5c6
Carlos Silva <carlos.silva@tech4h.com.br>
2022-06-06 01:22:23 -0300

    fix: fix the location files personal and remove unecessary files

--- src/main/routes/personal-update-routes.ts
+++ src/main/routes/personal-update-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import ExpressRouterAdapter from '../adapter/express-route-adapter'
-import PersonalRouterComposer from '../composer/personal-composer'
+import PersonalRouterComposer from '../composer/personal/personal-composer'
 
 module.exports = (router: Router) => {
   router.put('/personal/:id', ExpressRouterAdapter.adapt(PersonalRouterComposer.composer()))
