28257567bcd1e8d26918ed7bacde8ea5ea65eeb8
Carlos Silva <carlos.silva@tech4h.com.br>
2022-06-07 02:32:20 -0300

    feat: add data student use case

--- src/domain/data-student/data-student-usecase.ts
+++ src/domain/data-student/data-student-usecase.ts
@@ -1,26 +1,23 @@
 import DataStudentRepositoryInterface from '../../interface/data-student/data-student-repository'
 import DataStudentModelInterface from '../../interface/data-student/data-student-model'
-import Encrypter from '../../interface/encrypter'
 import ErrorRes from '../../presentation/utils/error'
 
 export default class DataStudentUseCase {
   private dataStudentRepository
-  private encrypter
 
-  constructor (dataStudentRepository: DataStudentRepositoryInterface, encrypter: Encrypter) {
+  constructor (dataStudentRepository: DataStudentRepositoryInterface) {
     this.dataStudentRepository = dataStudentRepository
-    this.encrypter = encrypter
   }
 
-  async create (dataStudent: DataStudentModelInterface) {
-
-  }
-
-  async load (id: number) {
-
+  async list (id: number) {
+    if (!id) {
+      throw new ErrorRes(400, 'invalid field id!')
+    }
+    return this.dataStudentRepository.list(id)
   }
 
   async update (id: number, dataStudent: DataStudentModelInterface) {
-
+    const result = await this.dataStudentRepository.update(id, dataStudent)
+    return result
   }
 }
