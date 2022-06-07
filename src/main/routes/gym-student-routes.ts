187ab21fbf0d8543d509b79e60152d14d0b82eaa
LuanBMartins <luan.cbm@hotmail.com>
2022-06-07 01:17:39 -0300

    feat: add route list students for Personal

--- src/main/routes/gym-student-routes.ts
+++ src/main/routes/gym-student-routes.ts
@@ -4,10 +4,12 @@ import GymStudentCreateRouterComposer from '../composer/gym-student/gym-student-
 import GymStudentLoadCompose from '../composer/gym-student/gym-student-load-composer'
 import GymStudentDeleteRouterComposer from '../composer/gym-student/gym-student-delete-composer'
 import GymStudentupdateRoute from '../composer/gym-student/gym-student-update-composer'
+import GymStudentListAllRoute from '../composer/gym-student/gym-student-listAll-composer'
 
 module.exports = (router: Router) => {
   router.post('/gymstudent/create', ExpressRouterAdapter.adapt(GymStudentCreateRouterComposer.composer()))
   router.post('/gymstudent/load', ExpressRouterAdapter.adapt(GymStudentLoadCompose.composer()))
   router.post('/gymstudent/delete', ExpressRouterAdapter.adapt(GymStudentDeleteRouterComposer.composer()))
   router.post('/gymstudent/update/:id', ExpressRouterAdapter.adapt(GymStudentupdateRoute.composer()))
+  router.get('/gymstudent/list/:id', ExpressRouterAdapter.adapt(GymStudentListAllRoute.composer()))
 }
---
+++ src/presentation/routes/gym-student/listAll-route.ts
@@ -0,0 +1,35 @@
+import loadHttpRequest from '../../../interface/gym-student/gym-student-create-httpRequest'
+import gymStudentUseCaseInterface from '../../../interface/gym-student/gym-student-usecase'
+import response from '../../utils/responseBody'
+import Authenticate from '../../utils/Authenticate'
+
+export default class GymStudentLoadRoute extends Authenticate {
+  private gymStudentUseCase
+  constructor (gymStudentUseCase: gymStudentUseCaseInterface) {
+    super()
+    this.gymStudentUseCase = gymStudentUseCase
+  }
+
+  async route (httpRequest: loadHttpRequest) {
+    try {
+      if (!httpRequest.headers.authorization) {
+        return response(401, 'Unauthorized!')
+      }
+      const auth = await this.authenticate(httpRequest.headers.authorization)
+      if (!auth) return response(401, 'Unauthorized!')
+
+      console.log(httpRequest)
+
+      if (!httpRequest.params.id) {
+        return response(400, 'invalid id!')
+      }
+      const userLoad = await this.gymStudentUseCase.listStudentforPersonal(httpRequest.params.id)
+      if (userLoad) {
+        return response(200, userLoad)
+      }
+      return response(200, 'Usuário não encontrado!')
+    } catch (error) {
+      return response(error.status, error.message)
+    }
+  }
+}
