9f1ad4a033a0f777717022bc23d96e20924721b5
Carlos Silva <carlos.silva@tech4h.com.br>
2022-07-05 02:14:13 -0300

    fix: fix the params

--- src/interface/data-student/data-student-create-httpRequest.ts
+++ src/interface/data-student/data-student-create-httpRequest.ts
@@ -3,5 +3,5 @@ import DataStudentModel from './data-student-model'
 export default interface dataStudentCreateHttpRequest {
     body: DataStudentModel,
     headers: {authorization: string},
-    params: {}
+    params: {id: number}
 }
