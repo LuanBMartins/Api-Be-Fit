4c8ad82e8277aa0b03a0a834afc7b80699d9f305
Carlos Silva <carlos.silva@tech4h.com.br>
2022-07-06 12:10:48 -0300

    fix: fix bugs

--- src/infra/repositorios/data-student-repository.ts
+++ src/infra/repositorios/data-student-repository.ts
@@ -2,7 +2,7 @@ import DataStudent from '../database/models/data-student-model'
 
 export default class DataStudentRepository {
   list (id: number) {
-    return DataStudent.findOne({
+    return DataStudent.findAll({
       where: { gymStudentId: id },
       raw: true
     })
