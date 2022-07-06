2d5c22480c4c6146f55b8899a3e4fa52e02acee5
Carlos Silva <carlos.silva@tech4h.com.br>
2022-07-06 00:37:10 -0300

    fix: fix the route

--- src/main/routes/data-student-routes.ts
+++ src/main/routes/data-student-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 import ExpressRouterAdapter from '../adapter/express-route-adapter'
 import DataStudentLoadCompose from '../composer/data-student/data-student-load-composer'
-import DataStudentUpdateRoute from '../composer/data-student/data-student-create-composer'
+import DataStudentCreateRoute from '../composer/data-student/data-student-create-composer'
 
 module.exports = (router: Router) => {
   router.get('/datastudent/load/:id', ExpressRouterAdapter.adapt(DataStudentLoadCompose.composer()))
-  router.post('/datastudent/update/:id', ExpressRouterAdapter.adapt(DataStudentUpdateRoute.composer()))
+  router.post('/datastudent/create', ExpressRouterAdapter.adapt(DataStudentCreateRoute.composer()))
 }
