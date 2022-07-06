60966677cc1198520d8a542e96adf7a7e07270f4
Carlos Silva <carlos.silva@tech4h.com.br>
2022-07-06 11:49:30 -0300

    fix: fix the name route

--- src/main/routes/data-student-routes.ts
+++ src/main/routes/data-student-routes.ts
@@ -4,6 +4,6 @@ import DataStudentLoadCompose from '../composer/data-student/data-student-load-c
 import DataStudentCreateRoute from '../composer/data-student/data-student-create-composer'
 
 module.exports = (router: Router) => {
-  router.get('/datastudent/load/:id', ExpressRouterAdapter.adapt(DataStudentLoadCompose.composer()))
+  router.get('/datastudent/list/:id', ExpressRouterAdapter.adapt(DataStudentLoadCompose.composer()))
   router.post('/datastudent/create', ExpressRouterAdapter.adapt(DataStudentCreateRoute.composer()))
 }
